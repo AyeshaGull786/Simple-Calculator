df2b0fc887eab7767327f8943abc2421ec8b6c99
Ayesha Gull <160147961+AyeshaGull786@users.noreply.github.com>
2024-04-14 12:27:34 +0500

    push my calculator on Github

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+//printing a welcome message
+console.log("Welcome to Ayesha's Simple calculator");
+
+const answer = await inquirer.prompt([    
+  { message: "Enter your first number:", type: "number", name: "firstNumber" },
+  { message: "Enter your second :", type: "number", name: "secondNumber" },
+  {
+    message: "Select one operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+  },
+]);
+  //conditional statements
+  if (answer.operator=="ADDITION"){
+    console.log( answer.firstNumber + answer.secondNumber);
+  }else if(answer.operator=="SUBTRACTION"){
+    console.log(answer.firstNumber - answer.secondNumber);
+  }else if(answer.operator=="MULTIPLICATION"){
+    console.log(answer.firstNumber * answer.secondNumber);
+  }else if(answer.operator=="DIVISION"){
+    console.log(answer.firstNumber / answer.secondNumber);
+  }else{
+  console.log("Please select correct operator");
+  }
+
+
+ 
+
+
+
